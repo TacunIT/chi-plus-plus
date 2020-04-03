2a0eb98423f1ed67e96d2105861850c7cfb9b84e
Chi++ <git@chiplusplus.net>
2020-04-03 09:35:14 +0200

    Rivista struttura delle directory per includere direttamente gli script .cpp nel testo del Manuale

---
+++ src/cpp/7.1-operatore-incremento.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv)
+{
+    int C = 12;         // dichiara la variabile C 
+    C++;                // incrementa la variabile C
+    cout << C << endl;  // stampa il valore di C   
+    return 0;               
+} 
