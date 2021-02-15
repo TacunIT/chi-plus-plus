0b6a24939c3d80e0d74ea25d1ce465818385c189
Chi++ <git@chiplusplus.net>
2021-02-15 08:09:39 +0100

    Completato testo su ricorsione

--- src/cpp/funzioni-ricorsiva.cpp
+++ src/cpp/funzioni-ricorsiva.cpp
@@ -9,23 +9,22 @@
 using namespace std;
 
 /** Dichiarazione della funzione ricorsiva */
-void ricorsiva(int n);
+long fattoriale(int n);
 
 /** Funzione main prima della definizione */
 int main(int argc, char** argv)
 {   
     int n = 9;
-    ricorsiva(n);
-    cout << endl;
+    cout << n << "! = " << fattoriale(n) << endl;
     return 0;
 }
 
 /** Definizione della funzione ricorsiva */
-void ricorsiva(int n)
+long fattoriale(int n)
 {
-    /** Richiama sé stessa pr n volte */
-    if(n > 0) {
-        ricorsiva(n-1);
-        cout << setw(2) << n;
+    long fatt = 1;
+    if(n > 1) {
+        fatt = n * fattoriale(n-1);
     }
+    return fatt;
 }
