37c454056e488fc9977242094de07394f66d4565
Chi++ <git@chiplusplus.net>
2021-07-15 06:23:08 +0200

    Redazione del testo del capitolo sul debug

--- src/cpp/debug-gestione-errori.cpp
+++ src/cpp/debug-gestione-errori.cpp
@@ -14,6 +14,12 @@ using namespace std;
 #define ERR_FILE_NONE   -10
 #define ERR_FILE_OPEN   -20
 #define ERR_FILE_READ   -30
+#define LOG_DEBUG         1
+#define LOG_AVVISO        2
+#define LOG_ERRORE        3
+#define S_DEBUG         "DEBUG"
+#define S_AVVISO        "AVVISO"
+#define S_ERRORE        "ERRORE"
 #define S_ERR_FILE_NONE "Definire un file di input"
 #define S_ERR_FILE_OPEN "Impossibile aprire il file di input"
 #define S_ERR_FILE_READ "Impossibile leggere il file di input"
@@ -72,6 +78,34 @@ void errore(int codice, bool exit = true)
     }
 }
 
+/** 
+*   Questa è la funzione di log che abbiamo visto 
+*   nella lezione sulle funzioni.
+*/
+void log(int livello, int n_parametri, ...)
+{        
+    /** Definisce il livello del messaggio */
+    const char* s_livello; 
+    switch(livello) {
+        case LOG_DEBUG:  s_livello = S_DEBUG ; break;
+        case LOG_AVVISO: s_livello = S_AVVISO; break;
+        default:         s_livello = S_ERRORE; break;
+    }
+    
+    /** Scrive il testo del messaggio */
+    cerr << '[' << s_livello << "] ";
+
+    /** Scrive i parametri della lista */
+    va_list lista_parametri;
+    va_start(lista_parametri, n_parametri);
+    for(int p = 1; p <= n_parametri; p++) {        
+        cerr << va_arg(lista_parametri, char*) ;        
+    }    
+    va_end(lista_parametri);
+
+    cerr << endl;
+}
+
 /** Verifica la presenza dei parametri di avvio */
 int verifica_parametri(int argc, char** argv)
 {
@@ -81,7 +115,14 @@ int verifica_parametri(int argc, char** argv)
 /** Apre il file in lettura */
 int apri_file(ifstream& testo, const char* path)
 {
-    cout << "Apro il file: " << path << endl;
+/**
+*   Questo codice viene compilato solo se
+*   è definita la macro __LOG__
+*/    
+#ifdef __LOG__    
+    log(LOG_DEBUG, 2, "Apro il file: ",  path);
+#endif
+
     testo.open(path);
     return ERR_NONE;
 } 
@@ -126,14 +167,14 @@ int main(int argc, char** argv)
         *   Usa un blocco try/catch per intercettare una
         *   eventuale eccezione e gestirla in maniera
         *   omogenea al resto del codice.
-        *   Non può mettere solo una catch in fondo perché
+        *   Non può aggiungere una catch in fondo perché
         *   l'eccezione non dà informazioni sulla causa 
         *   dell'errore che l'ha generata.
         */
         try {
             testo.exceptions ( std::ifstream::badbit 
                              | std::ifstream::failbit );
-            esito = apri_file(testo, argv[1]);            
+            esito = apri_file(testo, argv[2]);            
         } catch(ifstream::failure e) {
             errore(ERR_FILE_OPEN);            
         }
