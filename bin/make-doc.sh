7b15e3f8f8ef9c4b3d6f7b442202a6b780fe0bae
Chi++ <git@chiplusplus.net>
2021-10-05 09:30:05 +0200

    Iniziato script di conversione del testo in PDF

---
+++ bin/make-doc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BASE_DIR=~/Developer/Chi++ 
+MAN_DIR=${BASE_DIR}/docs/man
+MAN_FILE=${BASE_DIR}/bin/man-file.txt
+
+cd ${MAN_DIR}
+
+pandoc $(<../../bin/man-file.txt) -s -t html --toc -c ../assets/css/screen.css -o merged.html
