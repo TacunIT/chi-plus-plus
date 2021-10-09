cab794c566dd5603b1d1b4ad23344462d5d005b1
Chi++ <git@chiplusplus.net>
2021-10-09 08:58:28 +0200

    Corretto file per la generazione del PDF

--- bin/make-doc.sh
+++ bin/make-doc.sh
@@ -3,7 +3,10 @@
 BASE_DIR=~/Developer/Chi++ 
 MAN_DIR=${BASE_DIR}/docs/man
 MAN_FILE=${BASE_DIR}/bin/man-file.txt
+CSS_FILE=${MAN_DIR}/assets/css/pdf.cs
+OUT_FILE=${MAN_DIR}/merged.html
 
 cd ${MAN_DIR}
 
-pandoc $(<../../bin/man-file.txt) -s -t html --toc -c ../assets/css/pdf.css -o merged.html
+echo "pandoc $(<${MAN_FILE}) -s -t html --toc -c ${CSS_FILE} -o ${OUT_FILE}"
+pandoc $(<${MAN_FILE}) -s -t html --toc -c ${CSS_FILE} -o ${OUT_FILE}
