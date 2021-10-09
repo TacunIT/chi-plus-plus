196671cf946d0866f08c4264836b2edbe063bc29
Chi++ <git@chiplusplus.net>
2021-10-09 08:46:03 +0200

    Aggiunto foglio di stile per PDF

--- bin/make-doc.sh
+++ bin/make-doc.sh
@@ -6,4 +6,4 @@ MAN_FILE=${BASE_DIR}/bin/man-file.txt
 
 cd ${MAN_DIR}
 
-pandoc $(<../../bin/man-file.txt) -s -t html --toc -c ../assets/css/screen.css -o merged.html
+pandoc $(<../../bin/man-file.txt) -s -t html --toc -c ../assets/css/pdf.css -o merged.html
