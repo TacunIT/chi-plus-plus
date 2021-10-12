e6cd3f586a9723701d831c3ae4a4176fa9b5347a
Chi++ <git@chiplusplus.net>
2021-10-12 10:10:25 +0200

    Rivisti stili pagina per omogeneità com documento unico

--- bin/make-doc.sh
+++ bin/make-doc.sh
@@ -4,9 +4,10 @@ BASE_DIR=~/Developer/Chi++
 MAN_DIR=${BASE_DIR}/docs/man
 MAN_FILE=${BASE_DIR}/bin/man-file.txt
 CSS_FILE=/assets/css/pdf.css
-OUT_FILE=${MAN_DIR}/merged.html
+OUT_DIR=${BASE_DIR}/_man
+OUT_FILE=${OUT_DIR}/merged.html
 
 cd ${MAN_DIR}
 
 echo "pandoc $(<${MAN_FILE}) -s -t html --toc -c ${CSS_FILE} -o ${OUT_FILE}"
-pandoc $(<${MAN_FILE}) -s -t html --toc -c ${CSS_FILE} -o ${OUT_FILE}
+pandoc $(<${MAN_FILE}) -s -t html --toc -c ${CSS_FILE} -o ${OUT_FILE} --metadata title="C'hi++"
