89801bc9061b66eebc210d5d638244f7976636dd
Chi++ <git@chiplusplus.net>
2021-12-17 08:15:24 +0100

    Aggiunti commenti allo script polimorfismo-algoritmi.cpp

--- src/cpp/polimorfismo-algoritmi.cpp
+++ src/cpp/polimorfismo-algoritmi.cpp
@@ -11,23 +11,28 @@ using namespace std;
 
 int main()
 {
+    /** Crea il vettore */
     vector<int> vect;
-    
+
+    /** Aggiunge dei valori al vettore */
     vect.push_back(10);
     vect.push_back(70);
     vect.push_back(21);
     vect.push_back(49);
     vect.push_back(35);
 
+    /** Mostra il valore più alto e il più basso */
     cout << *min_element(vect.begin(), vect.end()) << endl;
     cout << *max_element(vect.begin(), vect.end()) << endl;
     
+    /** Mostra i valori nell'ordine in cui sono stati inseriti */
     vector<int>::iterator i;
     for(i = vect.begin(); i != vect.end(); i++) {
         cout << *i << " ";
     }
     cout << endl;
 
+    /** Ordina i valori dal più basso al più alto e li visualizza */
     int n = vect.size();
     sort(vect.begin(), vect.end());
     for (int i=0; i<n; i++) {
@@ -35,6 +40,7 @@ int main()
     }
     cout << endl;
 
+    /** Ordina i valori dal più alto al più basso e li visualizza */
     reverse(vect.begin(), vect.end());
     for (int i=0; i<n; i++) {
         cout << vect[i] << " ";       
