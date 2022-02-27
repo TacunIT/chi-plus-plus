9ee859454d65a94f3de1c5781b6f2696a809d358
Chi++ <git@chiplusplus.net>
2022-02-27 09:18:29 +0100

    Prima prova di generazione di un e-book

--- _bin/make-doc.sh
+++ _bin/make-doc.sh
@@ -5,10 +5,10 @@ MAN_DIR=${BASE_DIR}/docs/man
 MAN_FILE=${BASE_DIR}/_bin/man-file.txt
 CSS_FILE=/assets/css/pdf.css
 OUT_DIR=${BASE_DIR}/_man
-OUT_FILE=${OUT_DIR}/pdf.html
+OUT_FILE=${OUT_DIR}/chiplusplus.pdf
 TEMPLATE=${BASE_DIR}/_layouts/pdf.html
 
 cd ${MAN_DIR}
 
 echo "pandoc $(<${MAN_FILE}) -s -t html --toc -c ${CSS_FILE} -o ${OUT_FILE} --template=${TEMPLATE} "
-pandoc $(<${MAN_FILE}) -s -t html --toc -c ${CSS_FILE} -o ${OUT_FILE} --template=${TEMPLATE} --section-divs
+pandoc $(<${MAN_FILE}) -s -t pdf --toc -c ${CSS_FILE} -o ${OUT_FILE} --template=${TEMPLATE}
