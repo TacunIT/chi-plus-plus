e6cf293e1638e11d4bc54d9b838d9177d8aaca8e
Chi++ <git@chiplusplus.net>
2023-02-21 15:07:58 +0100

    Revisione dello script per la generazione del PDF

--- bin/crea-ebook.sh
+++ bin/crea-ebook.sh
@@ -6,13 +6,19 @@ MAN_DIR=${BASE_DIR}/docs/man
 MAN_FILE=${BIN_DIR}/man-file.txt
 CSS_FILE=/assets/css/pdf.css
 OUT_DIR=${BASE_DIR}/_ebook
-OUT_FILE=${OUT_DIR}/chiplusplus.epub
+OUT_FILE=${OUT_DIR}/chiplusplus.pdf
 TEMPLATE=${BASE_DIR}/_layouts/pdf.html
 
 cd ${MAN_DIR}
-
+       
 #echo "pandoc $(<${MAN_FILE}) -s -t html --toc -c ${CSS_FILE} -o ${OUT_FILE} --template=${TEMPLATE} "
 #pandoc $(<${MAN_FILE}) -s -t pdf --toc -c ${CSS_FILE} -o ${OUT_FILE} --template=${TEMPLATE}
 
-echo "pandoc $(<${MAN_FILE}) -s -t html --toc -c ${CSS_FILE} -o ${OUT_FILE} "
-pandoc $(<${MAN_FILE}) -s -t pdf --toc  --extract-media="/Users/Carlo/Developer/Chi++" -c ${CSS_FILE} -o ${OUT_FILE}
+# pandoc $(<${MAN_FILE}) -s -t pdf --toc  --extract-media="/Users/Carlo/Developer/Chi++" -c ${CSS_FILE} -o ${OUT_FILE}
+       
+       
+pandoc --verbose                \
+       --pdf-engine=xelatex     \
+       -c ${CSS_FILE}           \
+       $(<${MAN_FILE})          \
+       -o ${OUT_FILE}
