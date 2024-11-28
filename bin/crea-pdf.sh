8a8ba73ce15569b9052e64ecb5a3848788ea5ca9
Chi++ <git@chiplusplus.net>
2024-11-28 08:17:34 +0100

    Modificato script per la generazione del PDF

--- bin/crea-pdf.sh
+++ bin/crea-pdf.sh
@@ -5,8 +5,9 @@ BIN_DIR=${BASE_DIR}/bin
 MAN_DIR=${BASE_DIR}/docs/man
 OUT_DIR=${BASE_DIR}/_book
 
-MAN_FILE=${BIN_DIR}/man-file.txt
-CSS_FILE=/assets/css/pdf.css
+MAN_FILE=${OUT_DIR}/man-file.txt
+CSS_FILE=${BASE_DIR}/_sass/pdf.css
+YML_FILE=${BASE_DIR}/_data/pdf.yml
 
 FILENAME="chi-plus-plus"
 FILE_NEW=$FILENAME".md"
@@ -17,35 +18,29 @@ DATA="$(date +'%d-%m-%Y')"
 cd ${MAN_DIR}
 
 echo "Genero il file PDF completo"
-pandoc  --verbose                            \
-        --pdf-engine=wkhtmltopdf             \
-        --pdf-engine-opt="--header-left"      --pdf-engine-opt="[title]"            \
-        --pdf-engine-opt="--header-right"     --pdf-engine-opt="[page] of [topage]" \
+pandoc  --verbose                 						\
+				--css ${CSS_FILE}    				 					\
+				--title="C'hi++, il senso della vita" \
+        --pdf-engine=wkhtmltopdf  					  \
+				--pdf-engine-opt="--print-media-type" \
+        --pdf-engine-opt="--header-left"      --pdf-engine-opt="C'hi++, il senso della vita è il debug"\
+        --pdf-engine-opt="--header-right"     --pdf-engine-opt="[page] di [topage]" \
         --pdf-engine-opt="--header-font-name" --pdf-engine-opt="Bookerly"           \
         --pdf-engine-opt="--header-font-size" --pdf-engine-opt="11"                 \
         --pdf-engine-opt="--header-spacing"   --pdf-engine-opt="10"                 \
         --pdf-engine-opt="--footer-left"      --pdf-engine-opt="Carlo Simonelli"    \
-        --pdf-engine-opt="--footer-right"     --pdf-engine-opt="[date]" \
-        --pdf-engine-opt="--footer-font-name" --pdf-engine-opt="Bookerly"           \
-        --pdf-engine-opt="--footer-font-size" --pdf-engine-opt="11"                 \
+        --pdf-engine-opt="--footer-right"     --pdf-engine-opt="[date]"             \
+        --pdf-engine-opt="--footer-font-name" --pdf-engine-opt="Simonetta"          \
+        --pdf-engine-opt="--footer-font-size" --pdf-engine-opt="10"                 \
         --pdf-engine-opt="--footer-spacing"   --pdf-engine-opt="10"                 \
         --pdf-engine-opt="--margin-top"       --pdf-engine-opt="3cm"                \
         --pdf-engine-opt="--margin-right"     --pdf-engine-opt="2cm"                \
         --pdf-engine-opt="--margin-bottom"    --pdf-engine-opt="3cm"                \
         --pdf-engine-opt="--margin-left"      --pdf-engine-opt="3cm"                \
 				--pdf-engine-opt="--page-size"        --pdf-engine-opt="A4"                 \
-        --css ./css/mammifero.css    \
-        $(<${MAN_FILE})              \
-        ./data/metadati.yml          \
-        -o ./$FILENAME.pdf
-
-prima=$(cat PAROLE)
-dopo=$(wc -w chi-plus-plus.md | awk '{print $1}' | tee PAROLE )
-diff=$((dopo-prima))
-echo "Parole aggiunte: $diff"
-echo "Parole totali  : $dopo"
-echo "Pagine stimate : $((dopo / 300))"
-echo $(wc -lwm chi-plus-plus.md | awk '{print $1, $2, $3}' )
+        $(<${MAN_FILE})             \
+				${YML_FILE}									\
+				-o ${OUT_DIR}/$FILENAME.pdf
 
 # rimuove i file .DS_Store"
 find . -type f -name .DS_Store -exec rm {} \;
