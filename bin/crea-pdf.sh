b2c3dc4ec5408714ecb24d1c8d684797cba01fb3
Chi++ <git@chiplusplus.net>
2025-06-28 08:43:18 +0200

    Rinominata directory _book in _pdf

--- bin/crea-pdf.sh
+++ bin/crea-pdf.sh
@@ -3,7 +3,7 @@
 BASE_DIR=~/Developer/Chi++
 BIN_DIR=${BASE_DIR}/bin
 MAN_DIR=${BASE_DIR}/docs/man
-OUT_DIR=${BASE_DIR}/_book
+OUT_DIR=${BASE_DIR}/_pdf
 
 MAN_FILE=${OUT_DIR}/man-file.txt
 CSS_FILE=${BASE_DIR}/_sass/pdf.css
@@ -18,30 +18,30 @@ DATA="$(date +'%d-%m-%Y')"
 cd ${MAN_DIR}
 
 echo "Genero il file PDF completo"
-pandoc  --verbose                 						\
-				--css ${CSS_FILE}    				 					\
-        --pdf-engine=wkhtmltopdf  					  \
-				--pdf-engine-opt="--print-media-type" \
-				--pdf-engine-opt="--title" 					  --pdf-engine-opt="C'hi++, il senso della vita è il debug" \
+pandoc  --verbose                               \
+        --css ${CSS_FILE}                       \
+        --pdf-engine=wkhtmltopdf                \
+        --pdf-engine-opt="--print-media-type"   \
+        --pdf-engine-opt="--title"            --pdf-engine-opt="C'hi++, il senso della vita è il debug" \
         --pdf-engine-opt="--header-left"      --pdf-engine-opt="C'hi++, il senso della vita è il debug"\
         --pdf-engine-opt="--header-right"     --pdf-engine-opt="[page] di [topage]" \
-        --pdf-engine-opt="--header-font-name" --pdf-engine-opt="Bookerly"          \
+        --pdf-engine-opt="--header-font-name" --pdf-engine-opt="Bookerly"           \
         --pdf-engine-opt="--header-font-size" --pdf-engine-opt="10"                 \
         --pdf-engine-opt="--header-spacing"   --pdf-engine-opt="10"                 \
         --pdf-engine-opt="--footer-left"      --pdf-engine-opt="Carlo Simonelli"    \
         --pdf-engine-opt="--footer-right"     --pdf-engine-opt="[date]"             \
-        --pdf-engine-opt="--footer-font-name" --pdf-engine-opt="Bookerly"          \
+        --pdf-engine-opt="--footer-font-name" --pdf-engine-opt="Bookerly"           \
         --pdf-engine-opt="--footer-font-size" --pdf-engine-opt="10"                 \
         --pdf-engine-opt="--footer-spacing"   --pdf-engine-opt="10"                 \
         --pdf-engine-opt="--margin-top"       --pdf-engine-opt="3cm"                \
         --pdf-engine-opt="--margin-right"     --pdf-engine-opt="2cm"                \
         --pdf-engine-opt="--margin-bottom"    --pdf-engine-opt="3cm"                \
         --pdf-engine-opt="--margin-left"      --pdf-engine-opt="3cm"                \
-				--pdf-engine-opt="--page-size"        --pdf-engine-opt="A4"                 \
-				--pdf-engine-opt='cover'              --pdf-engine-opt="${OUT_DIR}/copertina.html" \
-        $(<${MAN_FILE})             \
-				${YML_FILE}									\
-				-o ${OUT_DIR}/$FILENAME.pdf
+        --pdf-engine-opt="--page-size"        --pdf-engine-opt="A4"                 \
+        --pdf-engine-opt='cover'              --pdf-engine-opt="${OUT_DIR}/copertina.html" \
+        $(<${MAN_FILE})              \
+        ${YML_FILE}                  \
+        -o ${OUT_DIR}/$FILENAME.pdf
 
 # rimuove i file .DS_Store"
 find . -type f -name .DS_Store -exec rm {} \;
