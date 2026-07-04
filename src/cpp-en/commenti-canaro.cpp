d27998e8651a1853b932897957ab3085d5b9ba73
Raimondo Canaro <rc@canaro.net>
2026-07-04 19:34:10 +0200

    Tradotto capitolo Commenti

--- src/cpp-en/commenti-canaro.cpp
+++ src/cpp-en/commenti-canaro.cpp
@@ -1,32 +1,32 @@
 /**********************************************************
- 
+
 #include <iostream.h>
 
 #define ERR_NOFILE 1
 
-int errore( int errore );
-void leggi_file( char * nomefile );
-void crea_matrice(void);
-int inizializza(void);
-void imposta_probabilita(void);
-void attrattore(void);
-int chiudi(void);
+int error( int error );
+void read_file( char * filename );
+void create_matrix(void);
+int initialize(void);
+void set_probability(void);
+void attractor(void);
+int close(void);
 
 int main(int argc, char * argv[])
 {
-    int esito = 0 ;
+    int outcome = 0 ;
     if(argc < 2) {
-        esito = errore(ERR_NOFILE);
+        outcome = error(ERR_NOFILE);
     } else {
-        leggi_file(argv[1]);
-        crea_matrice();
-        if(inizializza()) {
-            const char * valore = imposta_probabilita();
-            attrattore(valore);
-            esito = chiudi();
+        read_file(argv[1]);
+        create_matrix();
+        if(initialize()) {
+            const char * value = set_probability();
+            attractor(value);
+            outcome = close();
         }
     }
-    return esito ;
+    return outcome ;
 }
 
 ***********************************************************/
