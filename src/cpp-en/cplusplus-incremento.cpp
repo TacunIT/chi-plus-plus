745e702365473deb2296177b453c68fad3d3aae4
Raimondo Canaro <rc@canaro.net>
2026-07-04 19:26:41 +0200

    Tradotto codice per capitolo cplusplus

--- src/cpp-en/cplusplus-incremento.cpp
+++ src/cpp-en/cplusplus-incremento.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    int C = 12;                  
-    cout << "Valore di C = " << C << endl;  
-    C++;                         
-    cout << "Valore di C = " << C << endl; 
-    return 0;               
-} 
+    int C = 12;
+    cout << "C value = " << C << endl;
+    C++;
+    cout << "C value = " << C << endl;
+    return 0;
+}
